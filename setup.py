7d37b93aeb72fc5f8ba7397cd25b17dadc044edd
clairemcwhite <claire.mcwhite@utexas.edu>
2016-07-13 13:56:47 -0500

    make setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='flupan',
+      version='0.1',
+      description='Python library to parse influenza passaging annotations',
+      url='http://github.com/clauswilke/flupan',
+      author='Claire D. McWhite',
+      author_email='claire.mcwhite@utexas.edu',
+      license='MIT',
+      package_dir={'flupan':'src'},
+      install_requires=['Biopython'],
+      packages=['flupan']
+     )
